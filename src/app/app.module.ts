422dfa4a1c1ee1d4a68e145cf5f0d5a1b0e96201
llSilverDev <67278780+lSilverDev@users.noreply.github.com>
2023-03-20 14:32:29 -0300

    style and sort

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile/profile.component';
+import { SummaryComponent } from './profile/summary/summary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule,
